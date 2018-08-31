a1a6fadde9b808f054a9f8517132107f5e97e428
Chris Atkeson <ctatkeson@gmail.com>
2018-08-30 22:43:49 -0700

    basics

--- app/forms.py
+++ app/forms.py
@@ -5,3 +5,4 @@ from wtforms.validators import DataRequired
 class LoginForm(FlaskForm):
     username = StringField('Username', validators=[DataRequired()])
     password = PasswordField('Password', validators=[DataRequired()])
+    submit = SubmitField('Sign In')
