8aace017d1f43558b8e4f5e00ef31d93530ec4c0
Chris Atkeson <ctatkeson@gmail.com>
2018-09-17 00:11:23 -0700

    Ajax Table Replacement

--- app/routes.py
+++ app/routes.py
@@ -6,6 +6,13 @@ from app import db
 from app.forms import LoginForm, HouseholdForm, Fee_StructureForm
 from app.models import User, Account, Household, Billing_Group, Fee_Structure
 from app.content import account_view, household_view, fee_view, dev_view
+import datetime,decimal
+
+def alchemyencoder(obj):
+	if isinstance(obj, datetime.date):
+		return obj.isoformat()
+	elif isinstance(obj, decimal.Decimal):
+		return '${:,.2f}'.format(obj)
 
 @app.route('/')
 def main():
@@ -37,26 +44,42 @@ def logout():
 def dashboard():
 	return render_template('dashboard.html')
 
-@app.route('/household/',methods=['GET', 'POST'])
+@app.route('/household/')
 @login_required
 def household():
+	return render_template('household_display.html',cols = household_view)
+
+@app.route('/household_data/')
+@login_required
+def household_data():
 	household_query=db.session.query(func.sum(Account.balance).label('balance'),Household.name.label('household_name'),func.min(Account.opening_date).label('opening_date'), \
 	func.count(Account.id).label('num_accounts'), Billing_Group.name.label('billing_group')).outerjoin(Household, Account.household_id == Household.id) \
 	.outerjoin(Billing_Group, Account.billing_group_id == Billing_Group.id).group_by(Household)
 
 	households=household_query.all()
-	return render_template('table_display.html',table=households, cols = household_view)
+	keys=households[0].keys()
+
+	data=[dict(zip([key for key in keys],row)) for row in households]
+	return (json.dumps({'data': data}, default = alchemyencoder))
 
 @app.route('/account/')
 @login_required
 def account():
+	return render_template('account_display.html', cols = account_view)
+
+@app.route('/account_data/')
+@login_required
+def account_data():
 	accounts_query = db.session.query(Account.name.label('account_name'),Account.account_number.label('account_number'), Account.custodian.label('custodian'), \
 	Account.opening_date.label('opening_date'), Account.balance.label('balance'), Household.name.label('household'),Billing_Group.name.label('billing_group'), \
 	Fee_Structure.name.label('fee_structure')).outerjoin(Household, Account.household_id == Household.id).outerjoin(Billing_Group, Account.billing_group_id == Billing_Group.id) \
 	.outerjoin(Fee_Structure, Account.fee_id == Fee_Structure.id)
 
 	accounts=accounts_query.all()
-	return render_template('table_display.html',table=accounts, cols = account_view)
+	keys=accounts[0].keys()
+
+	data=[dict(zip([key for key in keys],row)) for row in accounts]
+	return (json.dumps({'data': data}, default = alchemyencoder))
 
 @app.route('/fee_structure/',methods=['GET', 'POST'])
 @login_required
@@ -84,29 +107,37 @@ def edit_fee_structures(id):
 	return render_template('edit_fees.html')
 
 @app.route('/dev_data/')
+@login_required
 def dev_data():
 
-	accounts_query = db.session.query(Account.name.label('account_name'),Account.account_number.label('account_number'), Account.custodian.label('custodian'), \
-	Household.name.label('household'),Billing_Group.name.label('billing_group'), \
-	Fee_Structure.name.label('fee_structure')).outerjoin(Household, Account.household_id == Household.id).outerjoin(Billing_Group, Account.billing_group_id == Billing_Group.id) \
-	.outerjoin(Fee_Structure, Account.fee_id == Fee_Structure.id)
+	fee_structure_query = db.session.query(Fee_Structure.name.label('fee_name'),Fee_Structure.frequency.label('frequency'),Fee_Structure.collection.label('collection'), \
+	Fee_Structure.structure.label('structure'),Fee_Structure.valuation_method.label('valuation_method'),func.count(Account.id).label('num_accounts'), Fee_Structure.id.label('id')). \
+	outerjoin(Account, Account.fee_id == Fee_Structure.id).group_by(Fee_Structure.name)
 
-	accounts=accounts_query.all()
-	keys=accounts[0].keys()
+	fee_structures=fee_structure_query.all()
+	keys=fee_structures[0].keys()
 
-	data=[]
-
-	data=[dict(zip([key for key in keys],row)) for row in accounts]
-	data=json.dumps({'data':data})
+	data=[dict(zip([key for key in keys],row)) for row in fee_structures]
+	data=json.dumps({'data': data}, default = alchemyencoder)
 	
 	return data
 
-@app.route('/dev/')
+@app.route('/dev/',methods=['GET', 'POST'])
+@login_required
 def dev():
-	return render_template('dev.html',cols=dev_view)
+	if request.method == "POST" and request.json:
+		delete_keys = request.json
+		print(delete_keys)
+		delete_query = db.session.query(Fee_Structure).filter(Fee_Structure.id.in_(delete_keys))
+		delete_query.delete(synchronize_session=False)
+		db.session.commit()
+		return redirect(url_for('dev'))
+
+	return render_template('dev.html',cols=fee_view)
 
 
 @app.route('/fee_structure/create',methods=['GET', 'POST'])
+@login_required
 def create_fee():
 	form = Fee_StructureForm()
 	if form.validate_on_submit():
